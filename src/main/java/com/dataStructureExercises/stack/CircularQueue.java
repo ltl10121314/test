af9f121ac634cd414deace84e180c1ad05a60351
刘天龙 <liutianlong@bonc.com.cn>
2020-06-23 09:13:28 +0800

    增加循环队列

---
+++ src/main/java/com/dataStructureExercises/stack/CircularQueue.java
@@ -0,0 +1,61 @@
+package com.dataStructureExercises.stack;
+
+
+/**
+ * 循环队列
+ *
+ * @author Administrator
+ */
+public class CircularQueue {
+    /**
+     * 数组：items，数组大小：n
+     */
+    private String[] items;
+    private int n = 0;
+    /**
+     * head表示队头下标，tail表示队尾下标
+     */
+    private int head = 0;
+    private int tail = 0;
+
+    /**
+     * 申请一个大小为capacity的数组
+     *
+     * @param capacity
+     */
+    public CircularQueue(int capacity) {
+        items = new String[capacity];
+        n = capacity;
+    }
+
+    /**
+     * 入队
+     *
+     * @param item
+     * @return
+     */
+    public boolean enqueue(String item) {
+        // 队列满了
+        if ((tail + 1) % n == head) {
+            return false;
+        }
+        items[tail] = item;
+        tail = (tail + 1) % n;
+        return true;
+    }
+
+    /**
+     * 出队
+     *
+     * @return
+     */
+    public String dequeue() {
+        // 如果head == tail 表示队列为空
+        if (head == tail) {
+            return null;
+        }
+        String ret = items[head];
+        head = (head + 1) % n;
+        return ret;
+    }
+}
